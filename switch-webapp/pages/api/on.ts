cad184449717ff01ca9587670260308910e62c95
Theodor Lindberg <theodor.lindberg@gmail.com>
2022-01-19 19:02:53 +0100

    Update

---
+++ switch-webapp/pages/api/on.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  fetch("192.168.10.193:5000/on").then(() => console.log("Click"))
+  res.status(200).json({ name: 'On' })
+}
