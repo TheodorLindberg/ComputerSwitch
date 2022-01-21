03e58d59d905948a888e8635dc95a1ac912f1594
Theodor Lindberg <theodor.lindberg@gmail.com>
2022-01-21 22:10:29 +0100

    code fix

--- switch-webapp/pages/api/monitor/data.json.ts
+++ switch-webapp/pages/api/monitor/data.json.ts
@@ -11,11 +11,12 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
 
-  console.log(req.cookies)
-    if(req.query.code != process.env.code && req.cookies.code != process.env.code)
-      return res.status(403).send({message: "invalidCode"} as any);
+  const code = req.query.code || req.cookies.code;
 
-    const data = await fetch("http://192.168.10.228:3001/data.json")
-    const json = await data.json()
-    res.status(data.status).json(json)
+  if(code != process.env.code) {
+    return res.status(403).send({message: "invalidCode"} as any);
+  }
+  const data = await fetch("http://192.168.10.228:3001/data.json")
+  const json = await data.json()
+  res.status(data.status).json(json)
 }
